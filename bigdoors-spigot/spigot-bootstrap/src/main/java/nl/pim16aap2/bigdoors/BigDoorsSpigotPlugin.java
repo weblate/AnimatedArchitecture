e3961afa9886dcb732ff38027638af60e39c4d5d
Pim van der Loos <pimvanderloos@gmail.com>
2021-04-28 00:06:17 +0200

    Add new spigot-bootstrap module
    - Added new spigot-bootstrap module which packages the reuslting jar of spigot-core (and its compiled dependencies) into a jar inside its own jar.

---
+++ bigdoors-spigot/spigot-bootstrap/src/main/java/nl/pim16aap2/bigdoors/BigDoorsSpigotPlugin.java
@@ -0,0 +1,12 @@
+package nl.pim16aap2.bigdoors;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BigDoorsSpigotPlugin extends JavaPlugin
+{
+
+    public BigDoorsSpigotPlugin()
+    {
+    }
+
+}
