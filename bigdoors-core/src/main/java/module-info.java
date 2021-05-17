8ef9ba7a7fb572ea6645b192e7ed51fa84bacb5f
Pim van der Loos <pimvanderloos@gmail.com>
2021-05-17 23:36:56 +0200

    Use module system

---
+++ bigdoors-core/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module nl.pim16aap.bigdoors {
+    requires lombok;
+    requires java.logging;
+    requires org.jetbrains.annotations;
+    requires jdk.unsupported;
+    requires java.sql;
+    requires sqlite.jdbc;
+
+}
